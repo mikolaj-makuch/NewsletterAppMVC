bbdaa3c9977147545bcedd301319a67b380f25be
micuch11 <micuch1126@gmail.com>
2024-04-07 17:07:35 +0100

    MVC Update

---
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/SqlConnecction.cs
@@ -0,0 +1,12 @@
+﻿namespace NewsletterAppMVC.Controllers
+{
+    internal class SqlConnecction
+    {
+        private string connectionString;
+
+        public SqlConnecction(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+    }
+}
